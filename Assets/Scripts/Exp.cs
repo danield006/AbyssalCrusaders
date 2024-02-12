1feea0660e25d2bf8c606e677fc14b0a5808b810
Benson Chow <bca72@sfu.ca>
2024-02-11 22:53:34 -0800

    Added Exp and level counter in UI

--- Assets/Scripts/Exp.cs
+++ Assets/Scripts/Exp.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Exp : MonoBehaviour
 {
+    public TextMeshProUGUI expText;
+    public TextMeshProUGUI levelText;
     public int exp;
     public int level;
     public int cap;
@@ -12,6 +15,8 @@ public class Exp : MonoBehaviour
         exp = 0;
         level = 1;
         cap = Mathf.RoundToInt( 0.04f * (1 ^ 3) + 0.8f * (1 ^ 2) + 2 * 1);
+        expText.text = "Exp: " + exp.ToString() + "/" + cap.ToString();
+        levelText.text = "Level " + level.ToString();
     }
 
     void Update()
@@ -24,8 +29,18 @@ public class Exp : MonoBehaviour
         level++;
         exp = exp - cap;
         cap = Mathf.RoundToInt( 0.04f * (level ^ 3) + 0.8f * (level ^ 2) + 2 * level);
+
+        expText.text = "Exp: " + exp.ToString() + "/" + cap.ToString();
+        levelText.text = "Level " + level.ToString();
     }
-    public void setExp(int gain) {
-        exp += gain;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Entered collision with " + other.gameObject.name);
+        if(other.gameObject.tag == "Dropped") {
+            exp += 1;
+            expText.text = "Exp: " + exp.ToString() + "/" + cap.ToString();
+            Destroy(other.gameObject);
+        }
     }
 }
