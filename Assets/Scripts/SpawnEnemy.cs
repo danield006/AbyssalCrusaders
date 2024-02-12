52cf84f193a5f709c4f1c6e28a2bb26f14d545cd
Benson Chow <bca72@sfu.ca>
2024-02-11 23:12:43 -0800

    Added spawn enemies with middle click

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -12,12 +12,12 @@ public class SpawnEnemy : MonoBehaviour
     }
 
      public void spawnIn() {
-        if(Input.GetMouseButtonDown(3)) {
+        if(Input.GetMouseButtonDown(2)) {
             RaycastHit hit;
 
             if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity)) {
                 if(hit.collider.tag == "Ground") {
-                    Vector3 location = new Vector3(hit.point.x, hit.point.y + 3, hit.point.z);
+                    Vector3 location = new Vector3(hit.point.x, hit.point.y + 5, hit.point.z);
                     Instantiate(enemy, location, Quaternion.identity);
                 }
             }
